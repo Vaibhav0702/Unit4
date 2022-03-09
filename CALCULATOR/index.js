0bfa5f7a0bd397c8e348ac5c5c917a4ef3e19780
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-03-09 11:06:11 +0530

    Day 1 Assignment Express

---
+++ CALCULATOR/index.js
@@ -0,0 +1,15 @@
+const add = require("./add.JS");
+const subtract = require("./subtract");
+const multiply = require("./multiply");
+const divide = require("./divide");
+
+let a = 30;
+let b = 10;
+
+console.log(add(a,b));
+
+console.log(subtract(a,b));
+
+console.log(multiply(a,b));
+
+console.log(divide(a,b));
